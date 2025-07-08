d0ec9ebc0d5ea3d1de27e440d57e660240444238
SajjaPremsai <sajjapremsai8938@gmail.com>
2025-07-08 12:07:51 +0530

    added project related files and folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AsciiArt from './components/AsciiArt'
+import InputBox from './components/InputBox'
+
+function App() {
+  return (
+    <>
+      <AsciiArt />
+      <InputBox/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+const InputBox = () => {
+
+    return (
+        <div className="inputbox-container">
+            <h4>visitor@sajjapremsai.github.io.terminal:~$ </h4>
+            <input className="inputbox"/>
+        </div>
+    )
+}
+
+export default InputBox;
