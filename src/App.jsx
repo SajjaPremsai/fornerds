7f999abc96fd9290f838c82ce78b27254e5f4b0e
SajjaPremsai <sajjapremsai8938@gmail.com>
2025-07-08 14:17:39 +0530

    updated App.jsx and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import AsciiArt from './components/AsciiArt'
 import InputBox from './components/InputBox'
 
 function App() {
+  const [isClear,setisClear] = useState(false);
+  const handleClear = ()=>{
+    setisClear(true);
+  }
   return (
     <>
-      <AsciiArt />
-      <InputBox/>
+      {!isClear && <AsciiArt />}
+      <InputBox handleAscii={handleClear}/>
     </>
   )
 }
